25c64c6c06d20287ea7ab25a8cd296b6c13c0e93
Sebastian James Earl Alexander <109830911+Sebastian-JE-Alexander@users.noreply.github.com>
2025-01-31 17:10:02 +0000

    Delete snake-master_Mario directory

--- snake-master_Mario/src/frontend.h
+++
@@ -1,5 +0,0 @@
-#include <ncurses.h>
-//Handle keyboard commands in-game
-enum Direction get_next_move(enum Direction previous);
-//print character symbol in a list of points
-void display_points(PointList* snake, const chtype symbol);
